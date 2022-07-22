0ffc1f0074fca4c80d5113042ead328dd3b86987
rajiv-eulogik <rajiv.ranjan@eulogik.com>
2022-07-22 09:42:27 +0530

    email regex error skipped

--- src/dialogs/AddProcess/AddProcess.js
+++ src/dialogs/AddProcess/AddProcess.js
@@ -14,6 +14,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
 
 const _formDefault = {
   "title": {
