3e8a4aed5428ffb99db0790a9acfb52f06d3afed
Smit018 <smitagrawal789@gmail.com>
2023-06-06 18:27:11 +0530

    6 june

--- src/components/IOsources.jsx
+++ src/components/IOsources.jsx
@@ -1,19 +1,11 @@
-import React from 'react'
+import { React, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { IconButton } from 'evergreen-ui';
+import { CrossIcon } from 'evergreen-ui';
+import ClearIcon from '@mui/icons-material/Clear';
 
-const ComboBox=({label})=> {
-  return (
-    <Autocomplete className='w-60 bg-white'
-      disablePortal
-      id="combo-box-demo"
-      options={top100Films}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label={label} />}
-      freeSolo="true"
-    />
-  );
-}
+
 
 const top100Films = [
   { label: 'The Shawshank Redemption', year: 1994 },
@@ -27,22 +19,106 @@ const top100Films = [
   { label: 'Monty Python and the Holy Grail', year: 1975 },
 ];
 
-const IOsources = ({heading}) => {
+const Card = ({ selectedOption, close }) => {
+  return (
+    <div>
+      <div style={{width:"19vw",height:"12vh"}} className='bg-white shadow-sm justify-between flex items-center  hover:bg-blue-100 cursor-pointer'>
+        <h2 className='pl-3'>{selectedOption?.label}</h2>
+       
+        <ClearIcon className='pr-3' onClick={() => close(selectedOption?.label)}  />
+
+      </div>
+    </div>
+  )
+}
+
+const ComboBox = ({ label }) => {
+  const [collector, setCollector] = useState([])
+  const [inputValue, setInputValue] = useState('');
+
+
+  const handleOptionSelect = (event, option) => {
+    console.log(event, option)
+    if(option) {
+      const _arr = [...collector]
+      // CHECK ALREADY AVAIL
+      const found = _arr.filter(element => ( element?.label === option?.label ))
+      console.log('--found', found)
+      if(found?.length) {
+        // FOUND ALREADY AVAILABLE IN LIST
+        alert('already available')
+      }
+      else {
+        _arr.push(option)
+        setCollector([..._arr])
+        setInputValue('');
+      }
+   
+    }
+  }
+  const handleInputChange = (event, value) => {
+    setInputValue(value);
+  }
+
+  const handleClose = (value) => {
+    setInputValue('');
+    const _arr = [...collector]
+    const index = _arr.findIndex((element) => element.label === value)
+    _arr.splice(index, 1)
+    setCollector([..._arr])
+  }
+  
+  
+
+  return (
+    <div>
+      <div className='mb-4'>
+          {collector && collector.length ? collector.map((_opt, index) =>{ return (
+        <Card selectedOption={_opt} close={(value) => handleClose(value)} />)
+          }): null }
+   </div>
+
+      <Autocomplete className='w-60 bg-white'
+        disablePortal
+        id="combo-box-demo"
+        options={top100Films}
+        getOptionLabel={(option) => option?.label}
+        sx={{ width: 300 }}
+        renderInput={(params) => (<div>
+            <TextField {...params} label={label} onChange={handleInputChange} />
+          </div>)
+        }
+        freeSolo="true"
+        onChange={handleOptionSelect}
+        
+      />
+
+   
+    </div>
+
+  );
+};
+
+
+
+const IOsources = ({ heading }) => {
+
+
   return (
     <div className='ml-3'>
       <h1 className='mb-2 text-black text-2xl'>{heading}</h1>
       <div className='flex flex-wrap'>
         <div className='flex-1'>
-            <p className='py-4 text-sm'>Employees and vendors</p>
-            <ComboBox label="Add members and vendors"/>
+          <p className='py-4 text-sm'>Employees and vendors</p>
+          <ComboBox label="Add members and vendors" />
         </div>
         <div className='flex-1'>
-            <p className='py-4 text-sm'>Whatsapp groups</p>
-            <ComboBox label="Add whatsapp groups"/>
+          <p className='py-4 text-sm'>Whatsapp groups</p>
+          <ComboBox label="Add whatsapp groups" />
         </div>
         <div className='flex-1'>
-            <p className='py-4 text-sm'>Documents</p>
-            <ComboBox label="Add Documents"/>
+          <p className='py-4 text-sm'>Documents</p>
+          <ComboBox label="Add Documents" />
         </div>
       </div>
     </div>
@@ -50,3 +126,7 @@ const IOsources = ({heading}) => {
 }
 
 export default IOsources
+
+
+
+
